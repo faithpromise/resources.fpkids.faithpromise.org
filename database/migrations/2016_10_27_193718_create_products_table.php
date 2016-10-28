9ebb2d4ea67bdbf5000d0782d1c992683f355b54
Brad Roberts <brad@bradroberts.net>
2016-10-28 16:21:46 -0400

    Created DB table migrations

---
+++ database/migrations/2016_10_27_193718_create_products_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProductsTable extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up() {
+        Schema::create('products', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('category_id')->nullable();
+            $table->string('name', 100);
+            $table->text('description')->nullable();
+            $table->json('quantities')->nullable();
+            $table->json('options')->nullable();
+            $table->boolean('is_popular')->default(false);
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down() {
+        Schema::dropIfExists('products');
+    }
+}
